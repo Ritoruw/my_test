3b1613f0ead606f0e9765781c7ed2f6c398d02b8
Ritoruw <SSX511@yandex.ru>
2024-05-09 02:42:10 -0500

    task 4.1 done

--- exercises/04_data_structures/task_4_1.py
+++ exercises/04_data_structures/task_4_1.py
@@ -15,5 +15,6 @@
 
 """
 
-nat = "ip nat inside source list ACL interface GigabitEthernet0/2 overload"
+nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+nat =  nat.replace('FastEthernet','GigabitEthernet')
 print(nat)
